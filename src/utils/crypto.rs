abd4280a5684a493d89098cfc9ddf093a60f7467
hicaru <lich666black@gmail.com>
2023-04-24 15:44:47 +0500

    added encode

--- src/utils/crypto.rs
+++ src/utils/crypto.rs
@@ -1,28 +1,38 @@
+use crate::config::zilliqa::CHARSET;
+
 pub const GENERATOR: [u32; 5] = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
 
 pub fn polymod(values: &[u8]) -> u32 {
     let mut chk: u32 = 1;
+
     for p in values {
         let top = chk >> 25;
+
         chk = ((chk & 0x1ffffff) << 5) ^ (*p as u32);
+
         for i in 0..5 {
             if ((top >> i) & 1) == 1 {
                 chk ^= GENERATOR[i];
             }
         }
     }
+
     chk
 }
 
 pub fn hrp_expand(hrp: &str) -> Vec<u8> {
     let mut ret = Vec::new();
+
     for p in 0..hrp.len() {
         ret.push(hrp.as_bytes()[p] >> 5);
     }
+
     ret.push(0);
+
     for p in 0..hrp.len() {
         ret.push(hrp.as_bytes()[p] & 31);
     }
+
     ret
 }
 
@@ -38,6 +48,24 @@ pub fn create_checksum(hrp: &str, data: &Vec<u8>) -> Vec<u8> {
 
     for p in 0..6 {
         ret.push(((polymod >> (5 * (5 - p))) & 31) as u8);
+    }
+
+    ret
+}
+
+pub fn encode(hrp: &str, data: &Vec<u8>) -> String {
+    let checksum = create_checksum(hrp, data);
+    let combined = [&data[..], &checksum[..]].concat();
+    let mut ret = String::from(hrp) + "1"; // hrp is zil so it is zil1.
+
+    for p in 0..combined.len() {
+        let idx = combined[p] as usize;
+        let value = CHARSET.chars().nth(idx);
+
+        match value {
+            Some(v) => ret.push(v),
+            None => continue,
+        }
     }
 
     ret
@@ -70,4 +98,14 @@ fn test_create_checksum() {
     let should: Vec<u8> = vec![2, 14, 10, 20, 25, 19];
 
     assert_eq!(res, should);
+}
+
+#[test]
+fn test_encode() {
+    let hrp = "test";
+    let data = vec![128, 0, 64, 32];
+    let res = encode(hrp, &data);
+    let should = "test1qep0uve";
+
+    assert_eq!(should, res);
 }
