a3f5ab76884fead6fea5fc54d4d3e522fd628d75
hicaru <lich666black@gmail.com>
2023-04-24 15:23:07 +0500

    added checksum

--- src/utils/crypto.rs
+++ src/utils/crypto.rs
@@ -14,6 +14,35 @@ pub fn polymod(values: &[u8]) -> u32 {
     chk
 }
 
+pub fn hrp_expand(hrp: &str) -> Vec<u8> {
+    let mut ret = Vec::new();
+    for p in 0..hrp.len() {
+        ret.push(hrp.as_bytes()[p] >> 5);
+    }
+    ret.push(0);
+    for p in 0..hrp.len() {
+        ret.push(hrp.as_bytes()[p] & 31);
+    }
+    ret
+}
+
+pub fn create_checksum(hrp: &str, data: &Vec<u8>) -> Vec<u8> {
+    let mut values: Vec<u8> = Vec::new();
+
+    values.extend(hrp_expand(hrp));
+    values.extend(data);
+    values.extend(vec![0; 6]);
+
+    let polymod = polymod(&values) ^ 1;
+    let mut ret = Vec::new();
+
+    for p in 0..6 {
+        ret.push(((polymod >> (5 * (5 - p))) & 31) as u8);
+    }
+
+    ret
+}
+
 #[test]
 fn test_polymod() {
     let bytes: [u8; 16] = [
@@ -22,4 +51,23 @@ fn test_polymod() {
     let res = polymod(&bytes);
 
     assert_eq!(98216235, res);
+}
+
+#[test]
+fn test_hrp_expand() {
+    let test_str = "test";
+    let res = hrp_expand(test_str);
+    let should: Vec<u8> = vec![3, 3, 3, 3, 0, 20, 5, 19, 20];
+
+    assert_eq!(should, res);
+}
+
+#[test]
+fn test_create_checksum() {
+    let hrp = "test";
+    let data: Vec<u8> = vec![255, 64, 0, 0, 0, 2];
+    let res = create_checksum(hrp, &data);
+    let should: Vec<u8> = vec![2, 14, 10, 20, 25, 19];
+
+    assert_eq!(res, should);
 }
