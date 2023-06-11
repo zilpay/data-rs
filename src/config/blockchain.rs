43cb0d6cd01f1764905209965310132925e90231
hicaru <lich666black@gmail.com>
2023-06-11 12:32:19 +0500

    upgrade min block for start

--- src/config/blockchain.rs
+++ src/config/blockchain.rs
@@ -1,4 +1,4 @@
 pub const BLOCKCHAIN_KEY: &str = "BLOCKCHAIN";
 pub const BLOCK_NUMBER_KEY: &str = "BLOCK_NUMBER";
-pub const START_INDEX_BLOCK: u64 = 2688147;
+pub const START_INDEX_BLOCK: u64 = 2188355;
 pub const BLOCK_LIMIT: usize = 100;
